5c364a8709cb2f7b495463ee7eecf6778fc4e4b3
hsbsbwb <134806107+hsbsbwb@users.noreply.github.com>
2023-06-26 02:46:16 +0300

    Add files via upload

---
+++ sython.py
@@ -0,0 +1,90 @@
+import telethon
+from telethon import events
+from config import *
+import os
+import logging
+import asyncio
+import time
+from telethon.tl import functions, types
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+from telethon.utils import get_display_name
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.errors import FloodWaitError
+from telethon import TelegramClient, events
+from collections import deque
+from telethon import functions
+from telethon.errors.rpcerrorlist import (
+    UserAlreadyParticipantError,
+    UserNotMutualContactError,
+    UserPrivacyRestrictedError,
+)
+from telethon.tl.functions.channels import InviteToChannelRequest
+from telethon.tl.types import InputPeerUser
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl import functions
+from hijri_converter import Gregorian
+from telethon.tl.functions.channels import LeaveChannelRequest
+import base64
+import datetime
+from payment import *
+from help import *
+from checktele import *
+from telethon.tl.functions.messages import GetHistoryRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest
+import requests
+# -
+
+sython.start()
+c = requests.session()
+bot_username = '@zmmbot'
+bot_usernamee = '@A_MAN9300BOT'
+
+y = datetime.datetime.now().year
+m = datetime.datetime.now().month
+dayy = datetime.datetime.now().day
+day = datetime.datetime.now().strftime("%A")
+m9zpi = f"{y}-{m}-{dayy}"
+sec = time.time()
+
+LOGS = logging.getLogger(__name__)
+
+DEVS = [
+   1041483862,
+]
+DEL_TIME_OUT = 10
+normzltext = "1234567890"
+namerzfont = normzltext
+name = "Profile Photos"
+time_name = ["off"]
+time_bio = ["off"]
+
+
+async def join_channel():
+    try:
+        await sython(JoinChannelRequest("@RRJRJ"))
+    except BaseException:
+        pass
+
+
+
+
+@sython.on(events.NewMessage(outgoing=True, pattern=r"\.الاوامر"))
+async def _(event):
+    await event.edit(commands)
+
+@sython.on(events.NewMessage(outgoing=True, pattern=r"\.فحص"))
+async def _(event):
+    start = datetime.datetime.now()
+    await event.edit("جارٍ...")
+    end = datetime.datetime.now()
+    ms = (end - start).microseconds / 1000
+    await event.edit(f'''
+** 🐊WELCOME TO الدولة 🐊
+ VERSION : 3.3
+🐊 PING : `{ms}`
+🐊 DATE : `{m9zpi}`
+🐊 ID : `{event.sender_id}`
+🐊 SOURCE الدولة : @M_6_Qn**
+
+-قـم بأرسال `.الاوامر`
+''')
